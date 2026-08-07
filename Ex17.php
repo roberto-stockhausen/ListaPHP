b235177e4cb941779cd6794d531542ffc2775f34
Roberto Hille <roberto_hille@estudante.sesisenai.org.br>
2026-08-07 12:03:20 -0300

    add - part of tex checker

--- Ex17.php
+++ Ex17.php
@@ -1,11 +1,60 @@
 <?php
 
-$input = "My name is Yoshikage Kira. I'm 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married. I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest. I don't smoke, but I occasionally drink. I'm in bed by 11 PM, and make sure I get eight hours of sleep, no matter what. After having a glass of warm milk and doing about twenty minutes of stretches before going to bed, I usually have no problems sleeping until morning. Just like a baby, I wake up without any fatigue or stress in the morning. I was told there were no issues at my last check-up. I'm trying to explain that I'm a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night. That is how I deal with society, and I know that is what brings me happiness. Although, if I were to fight I wouldn't lose to anyone.";
+$input = "Johusaaysigdssy augdp7ga bas 7a9ga79";
 $output = processarTexto($input);
 
 function processarTexto($texto)
 {
-quantidadeTexto($texto);
+$palavrasFrases = quantidadeTexto($texto);
+$palavrasLongasCurtas = curtoLongo($texto);
+
+return[
+    "Caracteres" => $palavrasFrases["Tamanho"],
+    "Palavras" => $palavrasFrases["Palavras"],
+    "Frases" => $palavrasFrases["Frases"],
+    "MaisLonga" => $palavrasLongasCurtas["PalavraLonga"],
+    "MaisCurta" => $palavrasLongasCurtas["PalavraCurta"]
+];
+}
+
+function curtoLongo($texto)
+{
+$textoCaracteres = mb_strlen($texto);
+$counter = 0;
+$palavraAtual = "";
+$palavraLonga = "";
+$tamanhoPalavraLonga = 0;
+$palavraCurta = "";
+$tamanhoPalavraCurta = 120;
+$contadorPalavras = 0;
+while ($counter < $textoCaracteres)
+{
+if ($texto[$counter] != " ")
+{
+$palavraAtual = $palavraAtual . $texto[$counter];
+$contadorPalavras += 1;
+}
+else
+    {
+        if ($contadorPalavras > $tamanhoPalavraLonga)
+        {
+            $palavraLonga = $palavraAtual;
+            $tamanhoPalavraLonga = $contadorPalavras;
+        }
+        if ($contadorPalavras < $tamanhoPalavraCurta)
+        {
+            $palavraCurta = $palavraAtual;
+            $tamanhoPalavraCurta = $contadorPalavras;
+        }
+        $palavraAtual = "";
+        $contadorPalavras = 0;
+    }
+$counter += 1;
+}
+return[
+    "PalavraLonga" => $palavraLonga,
+    "PalavraCurta" => $palavraCurta
+];
 }
 
 function quantidadeTexto($texto)
@@ -34,6 +83,12 @@ while ($counter < $textoCaracteres)
     ];
 }
 
-echo "Caracteres no texto: " . $output["Tamanho"] . "<br>";
+
+
+echo "Caracteres no texto: " . $output["Caracteres"] . "<br>";
+echo "Palavras no texto: " . $output["Palavras"] . "<br>";
+echo "Frases no texto: " . $output["Frases"] . "<br>";
+echo "Palavra mais longa: " . $output["MaisLonga"] . "<br>";
+echo "Palavra mais curta: " . $output["MaisCurta"] . "<br>";
 
 ?>
