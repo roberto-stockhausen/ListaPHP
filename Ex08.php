f47d2d5417dc098d7baafb74d4d2c89aa40c099b
Roberto Hille <roberto_hille@estudante.sesisenai.org.br>
2026-08-07 10:51:59 -0300

    add - start of Ex08

---
+++ Ex08.php
@@ -0,0 +1,15 @@
+<?php
+
+$names = "Joshua do Bar, Giorno Giovanna, Deodoro da Fonseca, Carlos el Topo que gira, Single-seat twin engine supermaneuverable Sukhoi Su-35 Flanker-E-defense fighter, Sílvio Santos";
+
+function ordenarNomes($lista)
+{
+$counter = 0;
+$size = mb_strlen($lista);
+while ($counter < $size)
+{
+
+}
+};
+
+?>
